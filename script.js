9c0ffbcfbe1ef358020a1a84a011511c1ad96be7
Mark <103870627+Marrek1978@users.noreply.github.com>
2022-12-28 09:40:37 -0700

    init

---
+++ script.js
@@ -0,0 +1,38 @@
+// Failure is the opportunity to begin again more intelligently
+// verb             nount           phrase
+
+const verbs = [
+  'failure', 
+  'crashing', 
+  'flopping',
+  'falling',
+  'ruining',
+  'flunking'
+];
+
+const nouns = [
+  'opportunity', 
+  'possibility',
+  'opening', 
+  'fortune',
+  'moment',
+  'liberty'
+]
+
+const phrases = [
+  'to begin again more intelligently',
+  "to start anew with a better plan",
+  "to approach things differently",
+  "to try again with a better strategy",
+  "to start fresh with a more informed approach",
+  "to recommence with a smarter approach",
+  "to reevaluate and make changes for a more successful outcome"
+]
+
+
+const inspiratinalQuoteGenerator = () => {
+  console.log( 'hi')
+
+}
+
+inspiratinalQuoteGenerator()
